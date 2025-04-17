90fab688b70edec14214f327aae4159b6412e33d
aishaahmedjika <aisha.jika@aun.edu.ng>
2025-04-17 19:39:14 +0100

    aishanumber4.py

---
+++ Round_2/number4.py
@@ -0,0 +1,8 @@
+# number that display the second largest number
+input  [bool[10, 20, 4, 45,99]]
+
+# the second largest number is 45
+if 45 <= 45 :
+    print (bool("45"))
+    else:
+print ('the second largest number is 45')
